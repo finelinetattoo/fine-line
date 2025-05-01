f07ecc572303b6f8fb0810cf6dda097782dcc9b6
Victor Redondo <vrviktor@gmail.com>
2025-05-01 13:44:11 +0200

    chore(calendar): initial setup of calendar page and display component

---
+++ frontend/src/app/components/molecules/calendar-display/calendar-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-display',
+  imports: [],
+  templateUrl: './calendar-display.component.html',
+  styleUrl: './calendar-display.component.scss'
+})
+export class CalendarDisplayComponent {
+
+}
