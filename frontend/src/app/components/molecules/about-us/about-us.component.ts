6b7c5d4767876fe6ea7d07983f87a34ffe336ddc
Victor Redondo <vrviktor@gmail.com>
2025-05-03 13:30:16 +0200

    feat: create parallax About Fine Line section

---
+++ frontend/src/app/components/molecules/about-us/about-us.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../atoms/button/button.component';
+
+@Component({
+  selector: 'app-about-us',
+  imports: [ButtonComponent],
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.scss',
+})
+export class AboutUsComponent {}
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroCarouselComponent } from '../../components/molecules/hero-carousel/hero-carousel.component';
 import { CommonModule } from '@angular/common';
+import { AboutUsComponent } from '../../components/molecules/about-us/about-us.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, HeroCarouselComponent],
+  imports: [CommonModule, HeroCarouselComponent, AboutUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
