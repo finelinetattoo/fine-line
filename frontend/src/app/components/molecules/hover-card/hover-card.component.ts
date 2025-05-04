ca4756e9678c185d7e78c3786a965a42ef233ce8
Victor Redondo <vrviktor@gmail.com>
2025-05-04 12:06:41 +0200

    feat: add micropigmentation section with custom hover card and overlay illustration

--- frontend/src/app/components/molecules/hover-card/hover-card.component.ts
+++ frontend/src/app/components/molecules/hover-card/hover-card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hover-card',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './hover-card.component.html',
   styleUrl: './hover-card.component.scss',
 })
@@ -11,4 +12,5 @@ export class HoverCardComponent {
   @Input() imageMain!: string;
   @Input() imageOverlay!: string;
   @Input() imageAlt!: string;
+  @Input() customClass = '';
 }
