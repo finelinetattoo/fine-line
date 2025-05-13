2c8407eadd06f99e6741e1645bedfb592c4936cb
Victor Redondo <vrviktor@gmail.com>
2025-05-13 22:59:41 +0200

    feat(pages): add available-designs page with card-grid component

---
+++ frontend/src/app/features/available-designs/available-designs-page/available-designs-page.config.ts
@@ -0,0 +1,50 @@
+export const AvailableDesignsPageData = {
+  heroSection: {
+    title: 'Diseños de autor únicos e instransferibles.',
+  },
+  myGridItems: [
+    {
+      image: 'assets/images/designs/design-greek.webp',
+      title: '#1',
+      subtitle: 'Sculture artwork',
+    },
+    {
+      image: 'assets/images/designs/design-anatomy.webp',
+      title: '#2',
+      subtitle: 'Anatomia',
+    },
+    {
+      image: 'assets/images/designs/design-goddess.webp',
+      title: '#3',
+      subtitle: 'Paper tattoing',
+    },
+    {
+      image: 'assets/images/designs/design-sculpture-sketch.webp',
+      title: '#4',
+      subtitle: 'Boceto de escultura',
+    },
+    {
+      image: 'assets/images/designs/design-sculture.webp',
+      title: '#5',
+      subtitle: 'Escultura y Rosa',
+    },
+    {
+      image: 'assets/images/designs/design-women.webp',
+      title: '#6',
+      subtitle: 'Mujer con pájaros',
+    },
+    {
+      image: 'assets/images/designs/design-anatomy-two.webp',
+      title: '#7',
+      subtitle: 'Anatomia Hombre',
+    },
+  ],
+  wantGetTattoo: {
+    parallaxBgClass: 'bg-get-tattoo',
+    title: '¿Quieres hacerte un tatuaje?',
+    description:
+      'Te acompañamos en cada paso para que tu tatuaje sea una experiencia única.',
+    buttonLabel: 'Reserva tu cita',
+    buttonLink: '/reserva-cita',
+  },
+};
