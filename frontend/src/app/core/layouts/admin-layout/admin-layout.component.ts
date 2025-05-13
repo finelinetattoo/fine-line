da86ed0bdc4a627363ee998d77cbf4dcd846a9e1
Victor Redondo <vrviktor@gmail.com>
2025-05-13 12:36:45 +0200

    style(admin-layout): remove background in charts page

--- frontend/src/app/core/layouts/admin-layout/admin-layout.component.ts
+++ frontend/src/app/core/layouts/admin-layout/admin-layout.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterOutlet,
+  NavigationEnd,
+} from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
@@ -7,6 +12,8 @@ import { LogoComponent } from '../../../shared/components/logo/logo.component';
 import { AuthService } from '../../auth/auth-service/auth.service';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-admin-layout',
   imports: [
@@ -18,6 +25,7 @@ import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
     LogoComponent,
     NzDropDownModule,
     NzModalModule,
+    CommonModule,
   ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss',
@@ -27,9 +35,19 @@ export class AdminLayoutComponent {
   urlIconWeb = 'favicon.ico';
   private authService = inject(AuthService);
   private modal = inject(NzModalService);
+  private router = inject(Router);
   userName: string | null = null;
+  isChartsPage = false;
   ngOnInit(): void {
     this.userName = this.authService.getUsername();
+
+    const currentUrl = this.router.url;
+    this.isChartsPage = currentUrl.includes('/admin/estadisticas');
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.isChartsPage = event.url.includes('/admin/estadisticas');
+      });
   }
   logout(): void {
     this.authService.logout();
