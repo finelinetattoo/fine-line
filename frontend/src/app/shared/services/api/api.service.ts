7034c8f1f3ef18f02296bb5900ff9c9177168def
Victor Redondo <vrviktor@gmail.com>
2025-05-24 11:58:15 +0200

    feat: add SSR

--- frontend/src/app/shared/services/api/api.service.ts
+++ frontend/src/app/shared/services/api/api.service.ts
@@ -50,8 +50,10 @@ export class ApiService<T> {
 
     this.notificationService.error('Error', customError.message);
     if (error.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/admin/login';
+      if (typeof window !== 'undefined') {
+        localStorage.removeItem('token');
+        window.location.href = '/admin/login';
+      }
     }
     return throwError(() => customError);
   };
