cd1d8302b34fde3fb8afe69ca519e78a9307447b
Victor Redondo <vrviktor@gmail.com>
2025-05-25 20:45:47 +0200

    fix: update production API endpoint

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endpoint: 'https://fine-line-production.up.railway.app/api/',
+  endpoint: 'https://fine-line-production-cfd4.up.railway.app/api/',
   cloudinary: {
     cloudName: 'dwygcrj5r',
     uploadPreset: 'form_user_upload',
